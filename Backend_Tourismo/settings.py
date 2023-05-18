09c120bbf708656a5d3b8621bc80c7ffc4ee0acd
BenouaklilHodhaifa <kh_benouaklil@esi.dz>
2023-05-18 09:57:30 +0100

    cofig postgresql

--- Backend_Tourismo/settings.py
+++ Backend_Tourismo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'Backend_Tourismo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'rJ2FsyNVsWnluYCzfGa0',
+        'HOST': 'containers-us-west-180.railway.app',
+        'PORT': '7626',
     }
 }
 
